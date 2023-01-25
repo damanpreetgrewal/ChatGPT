f9d31803d454dd21dab00e7c9c2722583e517638
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-01-25 16:49:37 -0500

    Hero Page update

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.post('/', async (req, res) => {
   console.log(sampleData);
 
   let prePrompt =
-    'I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer.';
+    'I am a highly intelligent question answering PwC bot. If you ask me a question that is rooted in truth, I will give you the answer.';
   sampleData.forEach(obj => {
     prePrompt += 'Q :' + obj.Q + '\n' + 'A :' + obj.A + '\n\n';
   });
