9e97a9d78f1b17d94859c32b111955a1fdfb1df6
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-01-25 01:50:59 -0500

    React animation on responses using framer-motion

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -1,6 +1,8 @@
 import OpenAISVGLogo from '../icons/OpenAISVGLogo';
 import Hero from './Hero';
 import { motion } from 'framer-motion';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import { useState, useEffect } from 'react';
 
 // Primary Chat Window
 const ChatBox = ({ chatLog, setChatInput, handleSubmit, chatInput }) => {
@@ -37,6 +39,11 @@ const ChatBox = ({ chatLog, setChatInput, handleSubmit, chatInput }) => {
 
 // Individual Chat Message
 const ChatMessage = ({ message }) => {
+  const [text] = useTypewriter({
+    words: [message.message],
+    typeSpeed: 15,
+  });
+
   return (
     <motion.div
       // fade messages in
@@ -49,7 +56,10 @@ const ChatMessage = ({ message }) => {
           <div className={`avatar ${message.user === 'gpt' && 'chatgpt'}`}>
             {message.user === 'gpt' ? <OpenAISVGLogo /> : <div>You</div>}
           </div>
-          <div className='message'>{message.message}</div>
+          <div className='message'>
+            {' '}
+            {message.user === 'gpt' ? text.trimStart() : message.message}
+          </div>
         </div>
       </div>
     </motion.div>
